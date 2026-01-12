d3aeedfc336c56e469643ef0b661bc82d6e6ca41
nasnasrich <nasnasrich@gmail.com>
2026-01-11 16:29:13 -0800

    Fix imports and complete production build

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { CartProvider } from "./Context/CartContext"; // ✅ Import here
+import { CartProvider } from "./context/CartContext"; // ✅ Import here
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
