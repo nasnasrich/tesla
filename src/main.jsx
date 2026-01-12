9b71b15907352f9180b21ec7c245b42e12ba6910
nasnasrich <nasnasrich@gmail.com>
2026-01-12 00:22:34 -0800

    Fix CartContext import case sensitivity

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,15 @@
-
-// main.jsx or index.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { CartProvider } from "./context/CartContext"; // ✅ Import here
+import { CartProvider } from "./context/CartContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <CartProvider> {/* ✅ Wrap App with CartProvider */}
+      <CartProvider>
         <App />
       </CartProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
-
