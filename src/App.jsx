17915a8d1ec2b2648ca125cae91a26c7243a6350
nasnasrich <nasnasrich@gmail.com>
2026-01-12 00:50:23 -0800

    Fix Login import casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import DashBoard from "./Pages/DashBoard.jsx";
 import { Route, Routes } from "react-router-dom";
+
 import Nav from "./Component/Nav.jsx";
 import Card from "./Component/Card.jsx";
-import Login from "./Pages/login.jsx";
-import Registration from "./Pages/Registration.jsx";
 import Hero from "./Component/Hero.jsx";
 import Home from "./Component/Home.jsx";
 import CompanyReviews from "./Component/CompanyReviews.jsx";
@@ -11,43 +9,38 @@ import Vehicles from "./Component/Vehicles.jsx";
 import Formemployment from "./Component/Formemployment.jsx";
 import Ordernow from "./Component/Ordernow.jsx";
 import TeslaSalaries from "./Component/TeslaSalaries.jsx";
+import Offer from "./Component/Offer.jsx";
+import TrackingPage from "./Component/TrackingPage.jsx";
+import Login from "./Pages/Login.jsx";
+import Registration from "./Pages/Registration.jsx";
 import Cart from "./Pages/Cart.jsx";
 import Checkout from "./Pages/Checkout.jsx";
 import WomenCategory from "./Pages/WomenCategory.jsx";
-import Offer from "./Component/Offer.jsx";
-import TrackingPage from "./Component/TrackingPage.jsx";
-
-
-
-
-// import React from 'react'
 
 function App() {
   return (
     <>
-    <Nav/>
-  <Routes>
-  {/* <Route path='/' element = {<DashBoard/>}/> */}
-  <Route path='/' element = {<Login/>}/>
-  <Route path="/cart" element={<Cart />} />
-  <Route path='/Card' element = {<Card/>}/>
-  <Route path='/Checkout' element = {<Checkout/>}/>
-  <Route path='/Login' element = {<Login/>}/>
-  <Route path='/Registration' element = {<Registration/>}/>
-  <Route path='Hero' element = {<Hero/>}/>
-  <Route path='/Home' element = {<Home/>}/>
-  <Route path="/Companyreviews" element={<CompanyReviews />} />
-  <Route path='/Vehicles' element = {<Vehicles/>}/>
-  <Route path='/Formemployment' element = {<Formemployment/>}/>
-  <Route path='/Ordernow' element = {<Ordernow/>}/>
-  <Route path='/WomenCategory' element = {<WomenCategory/>}/>
-  <Route path='/TeslaSalaries' element = {<TeslaSalaries/>}/>
-  <Route path='/Offer' element = {<Offer/>}/>
-  <Route path='/TrackingPage' element = {<TrackingPage/>}/>
-
-   </Routes>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/card" element={<Card />} />
+        <Route path="/hero" element={<Hero />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/companyreviews" element={<CompanyReviews />} />
+        <Route path="/vehicles" element={<Vehicles />} />
+        <Route path="/formemployment" element={<Formemployment />} />
+        <Route path="/ordernow" element={<Ordernow />} />
+        <Route path="/womencategory" element={<WomenCategory />} />
+        <Route path="/teslasalaries" element={<TeslaSalaries />} />
+        <Route path="/offer" element={<Offer />} />
+        <Route path="/trackingpage" element={<TrackingPage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
